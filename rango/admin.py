4f279464a54b676c847b849a529fc3ffbe171369
“Maksym <2950374p@student.gla.ac.uk>
2025-02-14 03:02:31 +0000

    fourth commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+
